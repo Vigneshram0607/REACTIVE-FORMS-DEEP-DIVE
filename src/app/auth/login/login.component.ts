650583f6aacf2dc88dd8fd5b0bc2442cc422bd3a
vigneshram0706 <vigneshram0706@gmail.com>
2025-06-10 10:12:10 +0000

    Reactive form Setup

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports:[ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email : new FormControl('',{
+      validators: [Validators.email, Validators.required]
+    }),
+    password: new FormControl('',{
+      validators: [Validators.required,Validators.minLength(6)]
+    }),
+  });
+
+  get emailIsInvalid(){
+    return this.form.controls.email.untouched && this.form.controls.email.dirty && this.form.controls.email.invalid;
+  }
+  get git (){
+    return this.form.controls.password.untouched && this.form.controls.password.dirty && this.form.controls.password.invalid;
+  }
+
+  onSubmit(){
+    console.log(this.form);
+  }
+}
