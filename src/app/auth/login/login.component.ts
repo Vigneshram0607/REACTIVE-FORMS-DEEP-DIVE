7e54279ba29517295732a7ff87df2f0e24014626
vigneshram0706 <vigneshram0706@gmail.com>
2025-06-11 04:46:11 +0000

    Added sync Validators for email

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { of } from 'rxjs';
+
+function mustContainQuestionMark(control: AbstractControl){
+  if(control.value.includes('?')){
+    return null;
+  }
+  return { doesNotContainQuestionMark: true};
+}
+
+function emailIsUnique(control: AbstractControl){
+  // Return Observable for 'asyncValidators'
+  if(control.value !== 'test@example.com'){
+    return of(null);
+  }
+  return of({notUnique: true});
+}
+
 
 @Component({
   selector: 'app-login',
@@ -11,17 +28,18 @@ import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from 
 export class LoginComponent {
   form = new FormGroup({
     email : new FormControl('',{
-      validators: [Validators.email, Validators.required]
+      validators: [Validators.email, Validators.required],
+      asyncValidators:[emailIsUnique]
     }),
     password: new FormControl('',{
-      validators: [Validators.required,Validators.minLength(6)]
+      validators: [Validators.required,Validators.minLength(6), mustContainQuestionMark]
     }),
   });
 
   get emailIsInvalid(){
     return this.form.controls.email.untouched && this.form.controls.email.dirty && this.form.controls.email.invalid;
   }
-  get git (){
+  get passwordIsInvalid(){
     return this.form.controls.password.untouched && this.form.controls.password.dirty && this.form.controls.password.invalid;
   }
 
